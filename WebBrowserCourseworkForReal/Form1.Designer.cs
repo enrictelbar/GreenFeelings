1a5b8dfbf591a80a600ff5b8a7ca216b5a01e068
BBa-WMBP <BBa-WMBP@DESKTOP-RQF4JH7>
2016-10-20 16:46:32 +0400

    Add project files.

---
+++ WebBrowserCourseworkForReal/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace WebBrowserCourseworkForReal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.navigateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.goForwardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.goBackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.goHomeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addBookmarkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editBookmarksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.myBookmarksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonGO = new System.Windows.Forms.Button();
+            this.setAsHomePageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBoxHome = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.navigateToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1202, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setAsHomePageToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addBookmarkToolStripMenuItem,
+            this.editBookmarksToolStripMenuItem,
+            this.myBookmarksToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(94, 24);
+            this.editToolStripMenuItem.Text = "Bookmarks";
+            this.editToolStripMenuItem.Click += new System.EventHandler(this.editToolStripMenuItem_Click);
+            // 
+            // navigateToolStripMenuItem
+            // 
+            this.navigateToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.goForwardToolStripMenuItem,
+            this.goBackToolStripMenuItem,
+            this.goHomeToolStripMenuItem});
+            this.navigateToolStripMenuItem.Name = "navigateToolStripMenuItem";
+            this.navigateToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.navigateToolStripMenuItem.Text = "Navigate";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(206, 26);
+            this.quitToolStripMenuItem.Text = "Quit";
+            // 
+            // goForwardToolStripMenuItem
+            // 
+            this.goForwardToolStripMenuItem.Name = "goForwardToolStripMenuItem";
+            this.goForwardToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.goForwardToolStripMenuItem.Text = "Go Forward";
+            // 
+            // goBackToolStripMenuItem
+            // 
+            this.goBackToolStripMenuItem.Name = "goBackToolStripMenuItem";
+            this.goBackToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.goBackToolStripMenuItem.Text = "Go Back";
+            // 
+            // goHomeToolStripMenuItem
+            // 
+            this.goHomeToolStripMenuItem.Name = "goHomeToolStripMenuItem";
+            this.goHomeToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.goHomeToolStripMenuItem.Text = "Go Home";
+            // 
+            // addBookmarkToolStripMenuItem
+            // 
+            this.addBookmarkToolStripMenuItem.Name = "addBookmarkToolStripMenuItem";
+            this.addBookmarkToolStripMenuItem.Size = new System.Drawing.Size(239, 26);
+            this.addBookmarkToolStripMenuItem.Text = "Bookmark Current Page";
+            // 
+            // editBookmarksToolStripMenuItem
+            // 
+            this.editBookmarksToolStripMenuItem.Name = "editBookmarksToolStripMenuItem";
+            this.editBookmarksToolStripMenuItem.Size = new System.Drawing.Size(239, 26);
+            this.editBookmarksToolStripMenuItem.Text = "Edit Bookmarks";
+            // 
+            // myBookmarksToolStripMenuItem
+            // 
+            this.myBookmarksToolStripMenuItem.Name = "myBookmarksToolStripMenuItem";
+            this.myBookmarksToolStripMenuItem.Size = new System.Drawing.Size(239, 26);
+            this.myBookmarksToolStripMenuItem.Text = "My Bookmarks";
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(326, 6);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(515, 22);
+            this.textBoxURL.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(276, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "URL :";
+            // 
+            // buttonGO
+            // 
+            this.buttonGO.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.buttonGO.Location = new System.Drawing.Point(847, 6);
+            this.buttonGO.Name = "buttonGO";
+            this.buttonGO.Size = new System.Drawing.Size(75, 23);
+            this.buttonGO.TabIndex = 3;
+            this.buttonGO.Text = "GO";
+            this.buttonGO.UseVisualStyleBackColor = true;
+            this.buttonGO.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // setAsHomePageToolStripMenuItem
+            // 
+            this.setAsHomePageToolStripMenuItem.Name = "setAsHomePageToolStripMenuItem";
+            this.setAsHomePageToolStripMenuItem.Size = new System.Drawing.Size(206, 26);
+            this.setAsHomePageToolStripMenuItem.Text = "Set As Home Page";
+            this.setAsHomePageToolStripMenuItem.Click += new System.EventHandler(this.setAsHomePageToolStripMenuItem_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(12, 35);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1178, 671);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.richTextBoxHome);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1170, 642);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Home";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1024, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add Tab";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1105, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Close Tab";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBoxHome
+            // 
+            this.richTextBoxHome.Location = new System.Drawing.Point(0, 0);
+            this.richTextBoxHome.Name = "richTextBoxHome";
+            this.richTextBoxHome.Size = new System.Drawing.Size(1179, 646);
+            this.richTextBoxHome.TabIndex = 0;
+            this.richTextBoxHome.Text = "";
+            this.richTextBoxHome.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1202, 718);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.buttonGO);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxURL);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem navigateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem goForwardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem goBackToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem goHomeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addBookmarkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editBookmarksToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem myBookmarksToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonGO;
+        private System.Windows.Forms.ToolStripMenuItem setAsHomePageToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBoxHome;
+    }
+}
+
---
+++ WebBrowserCourseworkForReal/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace WebBrowserCourseworkForReal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // Check if valid URI
+            Uri uriResult;
+            bool isUri = Uri.TryCreate(textBoxURL.Text, UriKind.Absolute, out uriResult)
+                && uriResult.Scheme == Uri.UriSchemeHttp;
+
+            // Do Request if valid URI
+            if (isUri) {
+
+                // Set up request object
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(textBoxURL.Text);
+
+                HttpStatusCode responseStatusCode;
+
+                // Try getting response
+                try
+                {
+                    // Setup response object
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    
+                    // Get the stream associated with the response.
+                    Stream receiveStream = response.GetResponseStream();
+
+                    // Pipes the stream to a higher level stream reader with the required encoding format. 
+                    StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+
+                    // Get the status code
+                    responseStatusCode = response.StatusCode;
+
+                    // Send the stream to the RichTextBox1
+                    richTextBoxHome.Text = "Response Code: "+(int)responseStatusCode+"\n-----------------\n"+readStream.ReadToEnd();
+
+                    // Close response and stream
+                    response.Close();
+                    readStream.Close();
+                }
+                catch(WebException ex)
+                {
+                    if (ex.Status == WebExceptionStatus.ProtocolError)
+                    {
+                        var response = ex.Response as HttpWebResponse;
+                        if (response != null)
+                        {
+                            richTextBoxHome.Text = "Response Code: " + (int)response.StatusCode + "\n--------------\nUnable to make a valid HTTP request, please check the URL";
+                        }
+                        else
+                        {
+                            // no http status code available
+                            richTextBoxHome.Text = "No Response Code\n--------------\nUnable to make a valid HTTP request, please check the URL";
+                        }
+                    }
+                    else
+                    {
+                        // no http status code available
+                        richTextBoxHome.Text = "No Response Code\n--------------\nUnable to make a valid HTTP request, please check the URL";
+                    }
+                }
+            }else
+            {
+                // In case URI is invalid
+                richTextBoxHome.Text = "'"+textBoxURL.Text+"' is not a valid URL, Please enter a valid URL";
+            }
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setAsHomePageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string title = "TabPage " + (tabControl1.TabCount + 1).ToString();
+            TabPage myTabPage = new TabPage(title);
+            tabControl1.TabPages.Add(myTabPage);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Remove(tabControl1.SelectedTab);
+        }
+
+        private void richTextBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
