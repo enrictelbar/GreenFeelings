5574e2eaf7f607a808e4810432eb3038589411b4
ericmartihaynes <ericmartihaynes@gmail.com>
2021-03-28 00:02:46 +0100

    pins

--- WebBrowserCourseworkForReal/Form2.cs
+++ WebBrowserCourseworkForReal/Form2.cs
@@ -33,6 +33,29 @@ namespace WebBrowserCourseworkForReal
             gMap.Zoom = 12;
             gMap.AutoScroll = true;
             gMap.Position = new PointLatLng(39.4702, -0.376805);
+            GMapOverlay markers = new GMapOverlay("markers");
+            GMapMarker marker1 = new GMarkerGoogle(new PointLatLng(39.4722, -0.376845), GMarkerGoogleType.green);
+            GMapMarker marker2 = new GMarkerGoogle(new PointLatLng(39.4712, -0.376875), GMarkerGoogleType.green);
+            GMapMarker marker3 = new GMarkerGoogle(new PointLatLng(39.4702, -0.376805), GMarkerGoogleType.green);
+            marker1.ToolTipText = "Arbol Grande";
+            marker1.ToolTip.Fill = Brushes.Black;
+            marker1.ToolTip.Foreground = Brushes.White;
+            marker1.ToolTip.Stroke = Pens.Black;
+            marker1.ToolTip.TextPadding = new Size(15, 15);
+            marker2.ToolTipText = "Arbol Mediano";
+            marker2.ToolTip.Fill = Brushes.Black;
+            marker2.ToolTip.Foreground = Brushes.White;
+            marker2.ToolTip.Stroke = Pens.Black;
+            marker2.ToolTip.TextPadding = new Size(15, 15);
+            markers.Markers.Add(marker1);
+            markers.Markers.Add(marker2);
+            markers.Markers.Add(marker3);
+            gMap.Overlays.Add(markers);
+            gMap.OnMarkerClick += new MarkerClick(gMap_OnMarkerClick);
+        }
+        private void gMap_OnMarkerClick(GMapMarker item, MouseEventArgs m)
+        {
+            Console.WriteLine("Hola");
         }
     }
 }
